070832043c88ea1c604dbf2f01e36d2584a510db
Fitriameliasitorus <fitamel86@gmail.com>
2025-01-22 14:09:19 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,32 @@
-public class Nomor1{
-     public static void main(String[] args) {
-        
+
+import java.util.Scanner;
+
+public class greenbird {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input jumlah kilometer yang ditempuh oleh penumpang
+        System.out.print("Masukkan total kilometer: ");
+        double kilometers = scanner.nextDouble();
+
+        // Menghitung biaya taksi
+        double fare = greenbird (kilometers);
+
+        // Output hasil perhitungan
+        System.out.printf("Total km: %.1f\nHonor taksi: %.1f\n", kilometers, fare);
+    }
+
+    private static double greenbird (double kilometers) {
+        double fare = 0;
+
+        // Biaya untuk 3 kilometer pertama atau kurang
+        if (kilometers <= 3) {
+            fare = 20000;
+        } else {
+            // Biaya untuk lebih dari 3 kilometer
+            fare = 20000 + (kilometers - 3) * 10000;
+        }
+
+        return fare;
     }
 }
